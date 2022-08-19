3f1617f00c800fddb1674bb22f7b7b1f4dd14198
Karlsson0214 <karlsson0214@gmail.com>
2022-08-19 09:28:21 +0200

    require RigidBody2D and BoxCollider2D

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 
 public class Player : MonoBehaviour
 {
